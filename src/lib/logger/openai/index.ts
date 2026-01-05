8e7bc73a0d019569170cdb17036b3e3f6efd79a5
roroghost17 <roroghost17@gmail.com>
2026-01-05 18:31:32 +0530

    [Maxim JS] OpenAI Realtime text integration

--- src/lib/logger/openai/index.ts
+++ src/lib/logger/openai/index.ts
@@ -2,4 +2,6 @@ export { MaximOpenAIClient } from "./client";
 export { MaximOpenAIChat } from "./chat";
 export { MaximOpenAIChatCompletions } from "./completions";
 export { MaximOpenAIResponses } from "./responses";
+export { MaximOpenAIRealtimeWrapper, wrapOpenAIRealtime } from "./realtime";
+export type { MaximRealtimeHeaders } from "./realtime";
 export { OpenAIUtils } from "./utils";
